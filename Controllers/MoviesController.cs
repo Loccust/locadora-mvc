0db88db15f3ceaa7352a547dd776a92a6c8154f9
Lucas Costa <lucasale00@hotmail.com>
2021-02-17 12:03:54 -0300

    Details - movie & Inicio Pargionação

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -11,27 +11,47 @@ using Microsoft.EntityFrameworkCore;
 
 namespace VideoStore.Controllers
 {
-  public class MoviesController : Controller
-  {
-    private readonly ILogger<MoviesController> _logger;
-    private readonly VideoStoreDbContext _context;
+    public class MoviesController : Controller
+    {
+        private readonly ILogger<MoviesController> _logger;
+        private readonly VideoStoreDbContext _context;
 
-    public MoviesController(VideoStoreDbContext context, ILogger<MoviesController> logger)
-    {
-      _context = context;
-      _logger = logger;
-    }
+        public MoviesController(VideoStoreDbContext context, ILogger<MoviesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
 
-    [HttpGet]
-    public async Task<IActionResult> ListMovies(int page)
-    {
-      var movies = await _context.Movies.OrderBy(movie => movie.Title).Skip(page-1).Take(8).ToListAsync();
-      return View(movies);
+        [HttpGet]
+        public async Task<IActionResult> ListMovies(int page)
+        {
+            try
+            {
+                ICollection<Movie> movies = await _context.Movies.OrderBy(movie => movie.Title).Skip(page - 1).Take(8).ToListAsync();
+                return View(movies);
+            }
+            catch (Exception e)
+            {
+                return View(e);
+            }
+        }
+        public async Task<IActionResult> MovieDetails(int id)
+        {
+            try
+            {
+                Movie movie = await _context.Movies.Where(movie => movie.MovieId == id).FirstOrDefaultAsync();
+                return View(movie);
+            }
+            catch (Exception e)
+            {
+                return View(e);
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-    public async Task<IActionResult> MovieDetails(int id)
-    {
-      var movie = await _context.Movies.Where(movie => movie.MovieId == id).FirstOrDefaultAsync();
-      return View(movie);
-    }
-  }
 }
